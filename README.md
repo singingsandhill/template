349697ff9f555e2935313103d21ab9300dc23793
Kim jisu <jisu0259@naver.com>
2025-05-14 14:48:20 +0900

    build: init project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # template
+
 바로인턴 java 직무 과제
---
+++ src/test/java/org/personal/template/TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.personal.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
